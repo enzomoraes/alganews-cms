35ee5156b8f8103a10b63162318e63d971f8e271
enzomoraes <enzomoraes12@hotmail.com>
2022-10-24 20:21:50 -0300

    chore: user performance and table features

--- src/sdk/services/Metric.service.ts
+++ src/sdk/services/Metric.service.ts
@@ -3,7 +3,15 @@ import Service from '../Service';
 
 class MetricService extends Service {
   static getTop3Tags() {
-    return this.Http.get<Metric.EditorTagRatio>('/metrics/editor/top3-tags').then(this.getData);
+    return this.Http.get<Metric.EditorTagRatio>(
+      '/metrics/editor/top3-tags'
+    ).then(this.getData);
+  }
+
+  static getEditorMonthlyEarnings() {
+    return this.Http.get<Metric.EditorMonthlyEarnings>(
+      '/metrics/editor/monthly-earnings'
+    ).then(this.getData);
   }
 }
 
